c6c2ca445b526a288180fb8454ed4929e05952f4
trongdev <hiiamtrong@gmail.com>
2021-02-22 12:16:55 +0700

    done base

--- app/controllers/auth.server.controller.js
+++ app/controllers/auth.server.controller.js
@@ -1,8 +1,6 @@
 const { CREDENTIALS } = require('../config/constants')
 const jwt = require('jsonwebtoken')
 const generateToken = ({ body, type }) => {
-  console.log(CREDENTIALS[`${type}_LIFE`])
-  console.log(CREDENTIALS[`${type}_SECRET`])
   return jwt.sign({ user: body }, CREDENTIALS[`${type}_SECRET`], {
     expiresIn: CREDENTIALS[`${type}_LIFE`],
   })
--- app/tests/app.test.js
+++ app/tests/app.test.js
@@ -1,9 +1,11 @@
 const { mongoose, connect, disconnect } = require('../db/mongoose')
+// eslint-disable-next-line no-undef
 beforeAll(async () => {
   await connect()
   await mongoose.connection.dropDatabase()
 })
 
+// eslint-disable-next-line no-undef
 afterAll(async () => {
   await disconnect(() => {
     console.log('Mongodb disconnect ')
