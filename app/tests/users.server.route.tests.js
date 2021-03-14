8f4afd147345c0a2eb3d4c8c1f9ad7255423339b
trongdev <hiiamtrong@gmail.com>
2021-03-14 18:53:08 +0700

    :gear: fix jest:not yet done

---
+++ app/tests/users.server.route.tests.js
@@ -0,0 +1,23 @@
+// const request = require('supertest')
+// const { app } = require('../app')
+
+// describe('users.server.route', () => {
+//   test('post users', async () => {
+//     try {
+//       const result = await request(app).post('/users').send({
+//         username: 'test',
+//         email: 'test@example.com',
+//       })
+//       console.log(result.status)
+//     } catch (err) {
+//       console.log(err)
+//     }
+//     // const result = await request(app)
+//     //   .post('/users')
+//     //   .send({
+//     //     username: 'test',
+//     //     email: 'test@example.com',
+//     //   })
+//     //   .catch((err) => {})
+//   })
+// })
