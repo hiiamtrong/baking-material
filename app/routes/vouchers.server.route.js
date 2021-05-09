5537d1926aa5e33fa27a10faba2c406d5b98ca18
trongdev <hiiamtrong@gmail.com>
2021-05-09 16:48:27 +0700

    init module voucher

---
+++ app/routes/vouchers.server.route.js
@@ -0,0 +1,10 @@
+const vouchers = require('../controllers/vouchers.server.controller')
+const { requireLogin } = require('../middlewares/auth.server.middleware')
+module.exports = (app) => {
+  app.use('/vouchers', requireLogin)
+  app.route('/vouchers').get(vouchers.list).post(vouchers.create)
+
+  app.route('/vouchers/:voucherId').get(vouchers.read).put(vouchers.update)
+
+  app.param('voucherId', vouchers.getRoleById)
+}
